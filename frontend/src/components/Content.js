aa2cd51de80abec5da2304b899afbacacd10d9fe
Niks-hs <20848206@students.latrobe.edu.au>
2023-10-17 14:47:14 +1100

    A2: 4th-Commit: frontend_draft_3

--- frontend/src/components/Content.js
+++ frontend/src/components/Content.js
@@ -5,11 +5,39 @@ function Task(props){
         props.setTasks(tasks => tasks.filter(task => task.id !== props.id))
     }
 
+    const[newType, setNewType] = useState("");
+    function onChangeType(event){
+        setNewType(event.target.value);
+ //       setNewNumber(event.target.value);
+    }
+    const[newNumber, setNewNumber] = useState("");
+    function onChangeNumber(event){
+        setNewNumber(event.target.value);
+    }
+    function onClickAdd(){
+        
+    }
+
     return(
-        <table style={{width:'350px', textAlign: 'left', alignItems: 'right', }}>
-            <td>{props.name}</td>
-            <td style={{marginLeft: 'auto'}}><button type = "button" onClick={onClick}>Delete</button></td>
-        </table>
+        <div style={{border:'1px solid #000', padding: '10px'}}>
+            <div style={{
+                display: 'flex', 
+                justifyContent: 'space-between', 
+                alignItems: 'center',
+                }}>
+            {/* </div><table style={{width:'350px', textAlign: 'left', alignItems: 'right', }}> */}
+                
+                {props.name}<button type = "button" onClick={onClick}>Delete</button>          
+                {/* <td style={{marginLeft: 'auto'}}></td> */}
+                
+            </div>
+            <hr/>
+            <div style={{display:'flex', justifyContent: 'space-between'}}>
+                <input type="text" placeholder="Phone Type" onChange={onChangeType}/>
+                <input type="text" placeholder="Phone Number" onChange={onChangeNumber}/>
+                <button type = "button" onClick = {onClickAdd}>Add</button>
+            </div>
+        </div>
     );
 }
 
@@ -20,7 +48,7 @@ function Content(props){
         setNewTask(event.target.value);
     }
     function onClick(){
-        props.setTasks(tasks => [...tasks, {id: tasks.length+1, name: newTask}])
+        props.setTasks(tasks => [...tasks, {id: tasks.length+1, name: newTask}]);
     }
     
     return(
@@ -31,18 +59,12 @@ function Content(props){
                 justifyContent: 'center', 
                 alignItems: 'center',
                 }}>
-                    <div style={{ textAlign: 'center', width: '400px', border: '1px solid #000', padding: '10px'}}>
+                    <div style={{ textAlign: 'center', width: '500px', border: '1px solid #000', padding: '10px'}}>
                         <h2>Contacts</h2>
                         <p><input type="text" placeholder="Name" onChange={onChange}/></p>
                         <button type = "button" onClick = {onClick}>Create Contact</button>
-                        <hr></hr>
-                        <div style={{
-                            display: 'flex', 
-                            justifyContent: 'space-between', 
-                            alignItems: 'left',
-                            border: '1px solid #000', padding: '10px'
-                            }}>                                
-                            <li style={{listStyleType:'none'}}>
+                        <hr></hr>             
+                        <li style={{listStyleType:'none'}}>
                                 {props.tasks.map(
                                     task => <Task
                                                 setTasks={props.setTasks}
@@ -50,8 +72,7 @@ function Content(props){
                                                 name={task.name}
                                             />
                                 )}
-                            </li>
-                        </div>
+                        </li>
                     </div>       
             </div>
         </div>
